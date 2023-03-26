be29c9582b53673cecd07a07cfdbee7d3b40f3a8
FotihYusupov <yusupovfotih158@gmail.com>
2023-03-26 21:25:24 +0500

    typeorm admin

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,13 +1,27 @@
 import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity({ name: 'admin' })
+@Entity({ name: 'admins' })
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({
+    type: 'varchar',
+    length: 32,
+    nullable: false
+  })
   user_name: string;
 
-  @Column()
+  @Column({
+    type: 'varchar',
+    length: '64',
+    nullable: false
+  })
   admin_password: string;
+
+  @Column({
+    type: 'enum',
+    nullable: false
+  })
+  user_role: 'SuperAdmin' | 'Admin'
 }
