800b48bf819427bce356eeaa12a31a9ee9ced209
FotihYusupov <102860161+FotihYusupov@users.noreply.github.com>
2023-04-13 18:05:11 +0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,16 +10,13 @@ import { CategoryModule } from './modules/category/category.module';
 import { Category } from './modules/category/category.entity';
 import { ShopsModule } from './modules/shops/shops.module';
 import { Shops } from './modules/shops/shops.entity';
-<<<<<<< HEAD
 import { ProductModule } from './modules/product/product.module';
 import { Product } from './modules/product/product.entity';
 import { ProductTypeModule } from './modules/product_type/product_type.module';
 import { ProductType } from './modules/product_type/product_type.entity';
-=======
 import { Driver } from './modules/drivers/driver.entity';
 import { TaklifModule } from './modules/takliflar/takliflar.module';
 import { Taklif } from './modules/takliflar/takliflar.entity';
->>>>>>> c5a2403b41307abb1b914ad9293a3aa93da20a83
 
 @Module({
   imports: [  
@@ -31,23 +28,16 @@ import { Taklif } from './modules/takliflar/takliflar.entity';
       port: 5432,
       host: process.env.PG_HOST,
       password: process.env.PG_PASSWORD,
-<<<<<<< HEAD
-      entities: [User, Category, Shops, Product, ProductType],
-=======
-      entities: [User, Category, Shops, Driver, Taklif],
->>>>>>> c5a2403b41307abb1b914ad9293a3aa93da20a83
+      entities: [User, Category, Shops, Product, ProductType, Driver, Taklif],
       synchronize: true,
     }),
     UserModule,
     DriversModule,
     CategoryModule,
     ShopsModule,
-<<<<<<< HEAD
     ProductModule,
     ProductTypeModule,
-=======
     TaklifModule
->>>>>>> c5a2403b41307abb1b914ad9293a3aa93da20a83
   ],
 })
 export class AppModule {}
